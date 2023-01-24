03961afd1e32ac8e917543e36d805ba3daf4d6d7
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-01-24 17:21:08 +0100

    chore: use node loader in vitest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { defineMessages } from '@formatjs/intl'
 import { NumberFormat } from '@formatjs/intl-numberformat/lib/src/core.js'
 import { describe, expect, it, vi } from 'vitest'
-import { createIntl, createIntlCache } from '..'
+import { createIntl, createIntlCache } from '../dist/index'
 
 import '../dist/forceNumberFormatPolyfill'
 import '@formatjs/intl-numberformat/locale-data/en'
